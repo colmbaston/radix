1b3b9d77cd238f9482288b3f26afef4a4b6cce04
Colm Baston <colm@colmbaston.uk>
2020-09-12 22:47:58 +0100

    don't initialise bucket and output vectors

--- src/lib.rs
+++ src/lib.rs
@@ -5,23 +5,29 @@ pub fn radix_sort(input : &mut Vec<u64>)
     const BUCKETS    : usize = 256;
     const MASK       : usize = BUCKETS - 1;
 
-    let mut buckets = vec![0usize ; BUCKETS];
-    let mut output  = vec![0u64   ; input.len()];
+    let mut buckets = Vec::with_capacity(BUCKETS);
+    let mut output  = Vec::with_capacity(input.len());
+
+    unsafe
+    {
+        buckets.set_len(BUCKETS);
+        output.set_len(input.len());
+    }
 
     for shift in (0 .. ITERATIONS).map(|i| i * RADIX)
     {
+        buckets.iter_mut().for_each(|x| *x = 0);
         input.iter().for_each(|k| buckets[*k as usize >> shift & MASK] += 1);
         buckets.iter_mut().scan(0, |a, x| { *a += *x; *x = *a; Some(()) }).last();
 
         for k in input.iter().rev()
         {
-            let index = &mut buckets[(*k as usize >> shift) & MASK];
+            let index = &mut buckets[*k as usize >> shift & MASK];
             *index -= 1;
             output[*index] = *k;
         }
 
         std::mem::swap(input, &mut output);
-        buckets.iter_mut().for_each(|x| *x = 0);
     }
 }
 
@@ -29,15 +35,14 @@ pub fn radix_sort(input : &mut Vec<u64>)
 mod tests
 {
     #[test]
-    fn random()
+    fn random_u64()
     {
-        let mut v = (0 .. 1_000_000).map(|_| rand::random()).collect();
-        super::radix_sort(&mut v);
-        assert!(is_sorted(&v));
-    }
+        let mut v = (0 .. 1_000_000).map(|_| rand::random()).collect::<Vec<u64>>();
+        let mut w = v.clone();
 
-    fn is_sorted(v : &[u64]) -> bool
-    {
-        v.iter().try_fold(u64::MIN, |a, x| if a <= *x { Some(*x) } else { None }).is_some()
+        super::radix_sort(&mut v);
+        w.sort();
+
+        assert_eq!(v, w);
     }
 }
